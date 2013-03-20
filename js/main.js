e346d0be9171e06ac7d73c2a1bce2a3cc9c3a343
Jentan Bernardus <jentanbernardus@gmail.com>
2013-03-20 04:56:48 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// To attach Backstrech as the body's backgroun
+ // $.backstretch("http://farm9.staticflickr.com/8175/7938796690_ed7034bb56_z.jpg");
+	 $("#header").backstretch("http://farm9.staticflickr.com/8175/7938796690_ed7034bb56_z.jpg");
+  // You may also attach Backstretch to a block-level element
+  //$(".foo").backstretch("path/to/image.jpg");
+
+  // Or, to start a slideshow, just pass in an array of images
+//  $("#header").backstretch([
+//    "http://distilleryimage9.s3.amazonaws.com/39aa14983f2411e2965422000a9f18cc_7.jpg",
+//    "http://farm9.staticflickr.com/8175/7938796690_ed7034bb56_z.jpg"    
+//  ],{centeredY: true, duration: 4000, fade: 750});
